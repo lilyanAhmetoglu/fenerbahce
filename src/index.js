4d2e6a907842be610da28249c48231b150bee313
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-01-20 13:47:28 +0300

    adding resources

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './Resources/Resources/css/app.css'
+import Routes from './routes';
+import {BrowserRouter} from 'react-router-dom'
+
+const App = () => {
+   return(
+       <BrowserRouter>
+          <Routes/>
+       </BrowserRouter>
+   )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
