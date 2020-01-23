192b590e6ada4effb22756bf808b6310987acf22
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-01-23 17:39:37 +0300

    finishing featured section

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -7,21 +7,21 @@ class Stripes extends Component {
     state = {
         stripes:[
             {
-                background:'#98c5e9',
+                background:'#000080',
                 left: 120,
                 rotate: 25,
                 top: -260 ,
                 delay: 0
             },
             {
-                background:'#ffffff',
+                background:'#FFFF00',
                 left: 360,
                 rotate: 25,
                 top: -397,
                 delay: 200
             },
             {
-                background:'#98c5e9',
+                background:'#000080',
                 left: 600,
                 rotate: 25,
                 top: -498,
@@ -37,14 +37,15 @@ class Stripes extends Component {
                 show={true}
 
                 start={{
-                    background:"#ffffff",
+                    background:'#ffffff',
                     opacity:0,
                     left:0,
                     rotate:0,
                     top:0
                 }}
+
                 enter={{
-                    background: [stripe.background],
+                    background:  `${stripe.background}`,
                     opacity:[1],
                     left:[stripe.left],
                     rotate:[stripe.rotate],
